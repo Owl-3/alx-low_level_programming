894b061968755e48579e7856ee2028866980795e
Owl-3 <d.odira@alustudent.com>
2021-07-05 04:11:05 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+void set_string(char **s, char *to); {
+    strcpy(point, "asd");
+}
+
+int main() {
+    char point[10];
+    ptrch(point);
+    printf("%s\n", point);
+    return 0;
+}
