917c8d870cbbbed2af1983e13a0260f705424744
Owl-3 <d.odira@alustudent.com>
2021-08-03 13:01:50 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,40 +2,40 @@
 
 /**
  * insert_nodeint_at_index - inserts a new node at a given position
- * @head: a pointer to the pointer to the first struct in the linked list
- * @idx: the index at which the new element has to be added
- * @n: the piece of data to put into the new node
- * Return: the address of the new node
+ * @head: double pointer to head of list
+ * @idx: index to add new node
+ * @n: value at new node
+ * Return: address of new node, or NULL if failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *temp, *temp2;
-	unsigned int i;
+	listint_t *current;
+	listint_t *newNode;
 
-	if (head == NULL)
+	register uint i;
+
+	newNode = malloc(sizeof(listint_t));
+	if (!head || !newNode)
 		return (NULL);
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = NULL;
-	if (idx == 0)
+	newNode->n = n;
+	if (!idx)
 	{
-		new->next = *head;
-		*head = new;
-		return (new);
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
 	}
-	temp = *head;
-	for (i = 0; temp->next && (i < (idx - 1)); i++, temp = temp->next)
-		;
-	if (idx > i + 1)
+	current = *head;
+	while (current)
 	{
-		free(new);
-		return (NULL);
+		if (i == idx - 1)
+		{
+			newNode->next = current->next;
+			current->next = newNode;
+			return (newNode);
+		}
+		i++;
+		current = current->next;
 	}
-	temp2 = temp->next;
-	temp->next = new;
-	new->next = temp2;
-	return (new);
+	free(newNode);
+	return (NULL);
 }
-c
