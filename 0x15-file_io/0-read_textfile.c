eac50f0f919fe1a048e30826fbf640e62f2f785b
Owl-3 <d.odira@alustudent.com>
2021-08-09 12:23:40 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,46 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the
- * POSIX standard output
- * @filename: pointer to filename
- * @letters: bytes to print
- * Return: actual bytes printed
+ * read_textfile - reads a text file and prints it to stdout
+ * @filename: the name of the file to be written
+ * @letters: the number of letters to read and print
+ * Return: the number of letters it read and printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	register int fd, r, w;
-	char *buffer = NULL;
+	int fd, numwrit, numread;
+	char *buf = malloc(sizeof(char) * (letters + 1));
 
-	if (!filename)
+	if (buf == NULL)
 		return (0);
-	buffer = malloc(letters + 1);
-	if (!buffer)
+	if (filename == NULL || letters == 0)
+	{
+		free(buf);
 		return (0);
+	}
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (fd < 0)
 	{
-		free(buffer);
-		return (0);
-	}
-	r = read(fd, buffer, letters);
-	if (r == -1)
-	{
-		free(buffer);
 		close(fd);
+		free(buf);
 		return (0);
 	}
-	buffer[letters] = '\0';
-	w = write(STDOUT_FILENO, buffer, r);
-	if (w == -1)
+	numread = read(fd, buf, letters);
+	if (numread < 0)
 	{
-		free(buffer);
 		close(fd);
+		free(buf);
 		return (0);
 	}
-	free(buffer);
+	buf[letters] = '\0';
+	numwrit = write(STDOUT_FILENO, buf, numread);
+	if (numwrit <= 0)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
 	close(fd);
-	return (w);
+	free(buf);
+	return (numwrit);
 }
